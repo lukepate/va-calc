ca40ed40f220fb70fc3ae7b70357bae078d17cf4
Luke Pate <luke.pate@ad.vetcompandpen.com>
2018-01-23 09:42:41 -0500

    add []

--- app/js/calc.js
+++ app/js/calc.js
@@ -1,12 +1,23 @@
 $(function() {
     console.log( "ready!" );
+    	var mono = [];
+    	var upperL = [];
+    	var upperR = [];
+    	var lowerL = [];
+	    var lowerR = [];
+
+      $("#ul, #ur, #ll, #lr").click(function(){
+          $(this).toggleClass('on');
+      });
+
+      
+
 });
 
 
+$('.number-pad').click(function() {
+  var val = $(this).attr("data-value");
+  var rate = val + "%"
+    $("p.rating").text(rate);
 
-    $('.number-pad').click(function() {
-      var val = $(this).attr("data-value");
-      var rate = val + "%"
-        $("p.rating").text(rate);
-
-    });
+});
